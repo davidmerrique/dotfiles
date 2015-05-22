8ddec5e21d6ab43ed0bef6bff2ff7a8253d0ed6e
David Merrique <david.merrique@gmail.com>
2015-05-22 14:10:55 -0400

    Added mov-to-gif

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -188,3 +188,12 @@ function updateall() {
   done
 
 }
+
+function mov-to-gif() {
+  # -r 10 tells ffmpeg to reduce the frame rate from 25 fps to 10
+  # -s 600x400 tells ffmpeg the max-width and max-height
+  # --delay=3 tells gifsicle to delay 30ms between each gif
+  # --optimize=3 requests that gifsicle use the slowest/most file-size optimization
+  # ffmpeg -i in.mov -s 600x400 -pix_fmt rgb24 -r 10 -f gif - | gifsicle --optimize=3 --delay=3 > out.gif
+  ffmpeg -i $1 -vf "scale=800:-1:flags=lanczos" -pix_fmt rgb24 -r 10 -f gif - | gifsicle --optimize=3 --delay=9 #
+}
