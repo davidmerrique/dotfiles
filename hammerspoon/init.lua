2799839a3a09335626c87115daa01e68f5d9c23d
David Merrique <david.merrique@gmail.com>
2015-07-20 14:53:54 -0400

    Use hammerspoon grid

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,85 +1,74 @@
+-- Import
+local grid = require 'hs.grid'
+
 -- Disable animation
 hs.window.animationDuration = 0
 
+grid.GRIDWIDTH = 12
+grid.GRIDHEIGHT = 12
+grid.MARGINX = 10
+grid.MARGINY = 10
+
+local gw = grid.GRIDWIDTH
+local gh = grid.GRIDHEIGHT
+
+local goLeft = {x = 0, y = 0, w = gw/2, h = gh}
+local goRight = {x = gw/2, y = 0, w = gw/2, h = gh}
+local goUp = {x = 0, y = 0, w = gw, h = gh/2}
+local goDown = {x = 0, y = gh/2, w = gw, h = gh}
+local goTopLeft = {x = 0, y = 0, w = gw/2, h = gh/2}
+local goTopRight = {x = gw/2, y = 0, w = gw/2, h = gh/2}
+local goBottomRight = {x = gw/2, y = gh/2, w = gw/2, h = gh/2}
+local goBottomLeft = {x = 0, y = gh/2, w = gw/2, h = gh/2}
+
+local goCenter = {x = 2, y = 1, w = 8, h = 10}
+local goFull = {x = 0, y = 0, w = gw, h = gh}
+
 -- Shortcuts
-local nudgeKey = {'alt', 'ctrl'}
-local yankKey = {'alt', 'ctrl', 'shift'}
-local pushKey = {'ctrl', 'alt', 'cmd'}
+local pushKey = {'alt', 'ctrl'}
+local resizeKey = {'alt', 'ctrl', 'shift'}
+local moveKey = {'ctrl', 'alt', 'cmd'}
 local pushShiftKey = {'shift', 'alt', 'cmd'}
 
--- Move a window a number of pixels x and y
-function nudge(xpos, ypos)
- local win = hs.window.focusedWindow()
- local f = win:frame()
- f.x = f.x + xpos
- f.y = f.y + ypos
- win:setFrame(f)
-end
-
--- Resize a window
-function yank(xpixels, ypixels)
- local win = hs.window.focusedWindow()
- local f = win:frame()
-
- f.w = f.w + xpixels
- f.h = f.h + ypixels
- win:setFrame(f)
-end
-
--- Resize window
-function push(x, y, w, h)
+-- Position a window in grid
+function gridSet(frame)
   local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  x = x / 100
-  y = y / 100
-  w = w / 100
-  h = h / 100
-
-  -- Add a margin
-  x = x + 0.01
-  y = y + 0.01
-  w = w - 0.02
-  h = h - 0.02
-
-  f.x = max.x + (max.w * x)
-  f.y = max.y + (max.h * y)
-  f.w = max.w * w
-  f.h = max.h * h
-  win:setFrame(f)
+  if win then
+    grid.set(win, frame, win:screen())
+  else
+    alert.show('No focused window')
+  end
 end
 
 -- Movement
-hs.hotkey.bind(nudgeKey, 'down', function() nudge(0, 100) end) -- down
-hs.hotkey.bind(nudgeKey, 'up', function() nudge(0, -100) end) -- up
-hs.hotkey.bind(nudgeKey, 'right', function() nudge(100, 0) end) -- right
-hs.hotkey.bind(nudgeKey, 'left', function() nudge(-100, 0) end) -- left
+hs.hotkey.bind(pushKey, 'down', hs.grid.pushWindowDown)
+hs.hotkey.bind(pushKey, 'up', hs.grid.pushWindowUp)
+hs.hotkey.bind(pushKey, 'right', hs.grid.pushWindowRight)
+hs.hotkey.bind(pushKey, 'left', hs.grid.pushWindowLeft)
 
 -- Resize
-hs.hotkey.bind(yankKey, 'up', function() yank(0, -100) end) -- yank bottom up
-hs.hotkey.bind(yankKey, 'down', function() yank(0, 100) end) -- yank bottom down
-hs.hotkey.bind(yankKey, 'right', function() yank(100, 0) end) -- yank right side right
-hs.hotkey.bind(yankKey, 'left', function() yank(-100, 0) end) -- yank right side left
+hs.hotkey.bind(resizeKey, 'up', hs.grid.resizeWindowShorter)
+hs.hotkey.bind(resizeKey, 'down', hs.grid.resizeWindowTaller)
+hs.hotkey.bind(resizeKey, 'right', hs.grid.resizeWindowWider)
+hs.hotkey.bind(resizeKey, 'left', hs.grid.resizeWindowThinner)
 
 -- Push to screen edge
-hs.hotkey.bind(pushKey, 'left', function() push(0, 0, 50, 100) end) -- left side
-hs.hotkey.bind(pushKey, 'right', function() push(50, 0, 50, 100) end) -- right side
-hs.hotkey.bind(pushKey, 'up', function() push(0, 0, 100, 50) end) -- top half
-hs.hotkey.bind(pushKey, 'down', function() push(0, 50, 100, 50) end) -- bottom half
+hs.hotkey.bind(moveKey, 'left', function() gridSet(goLeft) end)
+hs.hotkey.bind(moveKey, 'right', function() gridSet(goRight) end)
+hs.hotkey.bind(moveKey, 'up', function() gridSet(goUp) end)
+hs.hotkey.bind(moveKey, 'down', function() gridSet(goDown) end)
 
 -- Push to corner
-hs.hotkey.bind(pushShiftKey, 'up', function() push(0, 0, 50, 50) end) -- top left
-hs.hotkey.bind(pushShiftKey, 'right', function() push(50, 0, 50, 50) end) -- top right
-hs.hotkey.bind(pushShiftKey, 'down', function() push(50, 50, 50, 50) end) -- bottom right
-hs.hotkey.bind(pushShiftKey, 'left', function() push(0, 50, 50, 50) end) -- bottom left
+hs.hotkey.bind(pushShiftKey, 'up', function() gridSet(goTopLeft) end)
+hs.hotkey.bind(pushShiftKey, 'right', function() gridSet(goTopRight) end)
+hs.hotkey.bind(pushShiftKey, 'down', function() gridSet(goBottomRight) end)
+hs.hotkey.bind(pushShiftKey, 'left', function() gridSet(goBottomLeft) end)
 
 -- Center window
-hs.hotkey.bind(pushKey, 'c', function() push(14, 7, 72, 85) end)
+hs.hotkey.bind(moveKey, 'c', function() gridSet(goCenter) end)
 
 -- Fullscreen
-hs.hotkey.bind(pushKey, 'f', function() push(0, 0, 100, 100) end)
+hs.hotkey.bind(moveKey, 'f', function() gridSet(goFull) end)
 
 -- Auto reload config
 function reloadConfig(files)
