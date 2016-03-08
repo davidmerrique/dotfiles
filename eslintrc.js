9cec3bb9888f3075ec39927ce089febb2e9f6f9c
David Merrique <david.merrique@gmail.com>
2016-03-08 09:49:37 -0500

    Eslint

---
+++ eslintrc.js
@@ -0,0 +1,15 @@
+{
+  "extends": "airbnb/base",
+  "rules": {
+    "object-curly-spacing": [2, "never"],
+    "no-new": 0,
+    "comma-dangle": [1, "only-multiline"],
+    "max-len": 0,
+    "arrow-body-style": 0,
+    "no-console": 0,
+    "consistent-return": 0,
+    "quote-props": 0,
+    "no-param-reassign": 0,
+    "no-else-return": 0
+  }
+}
