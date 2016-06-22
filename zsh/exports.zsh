97994d732d983f69367eb6a1b4fe500b6926f357
David Merrique <david.merrique@gmail.com>
2016-06-22 07:56:52 -0400

    Cleanup

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -17,20 +17,5 @@ export PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
 # disable ._ file
 export COPYFILE_DISABLE=true
 
-# Man pages colors
-export LESS_TERMCAP_mb=$(tput bold; tput setaf 2)
-export LESS_TERMCAP_md=$(tput bold)
-export LESS_TERMCAP_me=$(tput sgr0)
-export LESS_TERMCAP_so=$(tput bold; tput rev)
-export LESS_TERMCAP_se=$(tput rmso; tput sgr0)
-export LESS_TERMCAP_us=$(tput setaf 4; tput bold)
-export LESS_TERMCAP_ue=$(tput sgr0)
-export LESS_TERMCAP_mr=$(tput rev)
-export LESS_TERMCAP_mh=$(tput dim)
-export LESS_TERMCAP_ZN=$(tput ssubm)
-export LESS_TERMCAP_ZV=$(tput rsubm)
-export LESS_TERMCAP_ZO=$(tput ssupm)
-export LESS_TERMCAP_ZW=$(tput rsupm)
-
 # Disable homebrew analytics
 export HOMEBREW_NO_ANALYTICS=1
--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -8,11 +8,6 @@ bindkey "^N" insert-last-word
 bindkey -s "^T" "^[Isudo ^[A" # "t" for "toughguy"
 
 # bind UP and DOWN arrow keys
-zmodload zsh/terminfo
-bindkey "$terminfo[kcuu1]" history-substring-search-up
-bindkey "$terminfo[kcud1]" history-substring-search-down
-
-# bind UP and DOWN arrow keys
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 
