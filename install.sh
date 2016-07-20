874cc7e2e1edcb7697544a13c1b66a197caf1ae9
David Merrique <david.merrique@gmail.com>
2016-07-20 16:35:04 -0400

    Tabify

--- install.sh
+++ install.sh
@@ -7,21 +7,21 @@ DEST=$HOME
 
 link() {
 	for i in $(find $1 -mindepth 1 -maxdepth 1); do
-	file=$DEST${i/$BASE/""}
+		file=$DEST${i/$BASE/""}
 
 		if [ ! -d $i ]; then
 			if [ -f $file -o -h $file ]; then
-		    echo "Replacing file: $file"
-		    rm $file
+				echo "Replacing file: $file"
+				rm $file
 			else
-		    echo "Creating link: $file"
+				echo "Creating link: $file"
 			fi
 
 			ln -s $i $file
 		else
 			if [ ! -d "$file" ]; then
 				echo "Creating directory: $file"
-			  mkdir $file
+				mkdir $file
 			fi
 
 			link $i
@@ -34,10 +34,10 @@ link $BASE
 
 # deleting stale links, if any
 for i in $HOME/.*; do
-  [ ! -h $i ] && continue
+	[ ! -h $i ] && continue
 
-  if [ ! -f $i ]; then
-    echo "Deleting stale link: $i"
-    rm $i
-  fi
+	if [ ! -f $i ]; then
+		echo "Deleting stale link: $i"
+		rm $i
+	fi
 done
