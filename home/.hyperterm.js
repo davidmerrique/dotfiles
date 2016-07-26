03b96798c89d4d485df80ab410a48793adeb8255
David Merrique <david.merrique@gmail.com>
2016-07-26 15:18:41 -0400

    Use hyperterm-overlay

--- home/.hyperterm.js
+++ home/.hyperterm.js
@@ -8,10 +8,26 @@ module.exports = {
       hotkey: 'Control+`',
       position: 'top'
     },
+    overlay: {
+      animate: false,
+      alwaysOnTop: true,
+      hasShadow: false,
+      hideOnBlur: false,
+      hideDock: false,
+      hotkeys: ['Control+`'],
+      resizable: true,
+      position: 'top',
+      primaryDisplay: true,
+      startup: true,
+      size: 0.4,
+      tray: true,
+      unique: false
+    }
   },
   plugins: [
     'hyperlinks',
-    'hyperterm-visor',
+    // 'hyperterm-visor',
+    'hyperterm-overlay',
     'hyperterm-title',
     'hyperterm-snazzy',
     'hyperterm-final-say'
