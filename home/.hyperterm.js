aaccfdaf713680830f660aac7e79dabac3bf608d
David Merrique <david.merrique@gmail.com>
2016-07-26 13:01:16 -0400

    BEAM cursor

--- home/.hyperterm.js
+++ home/.hyperterm.js
@@ -2,11 +2,12 @@ module.exports = {
   config: {
     fontSize: 18,
     fontFamily: '"Operator Mono", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    shell: 'zsh',
+    cursorShape: 'BEAM',
     visor: {
       hotkey: 'Control+`',
       position: 'top'
     },
-    shell: 'zsh'
   },
   plugins: [
     'hyperlinks',
