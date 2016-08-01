0829ab42e7f91a401f91dc9fda1759021296a846
David Merrique <david.merrique@gmail.com>
2016-08-01 12:20:16 -0400

    Cleanup

--- home/.zshrc
+++ home/.zshrc
@@ -7,9 +7,7 @@ zplug "sindresorhus/pure"
 zplug "djui/alias-tips"
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-completions"
-zplug "supercrabtree/k"
 zplug "rupa/z", use:z.sh
-zplug "mathiasbynens/dotfiles", as:command, use:".macos", rename-to:"macos"
 zplug "Jxck/dotfiles", as:command, use:bin/l
 zplug "zsh-users/zsh-syntax-highlighting", nice:10
 zplug "lukechilds/zsh-better-npm-completion", nice:12
--- zsh/vendor.zsh
+++ zsh/vendor.zsh
@@ -1,21 +1,6 @@
 # load rbenv if available
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
-# phpbrew
-# brew link icu4c gettext --force
-# phpbrew -d install 5.6.13 +default+dbs+mcrypt+iconv+icu+session+mcrypt+openssl
-# phpbrew -d install 5.6.13 +default+dbs+mcrypt+iconv+icu+session+mcrypt+openssl+gd -- --with-gd=shared --enable-gd-natf --with-jpeg-dir=/usr/local --with-png-dir=/usr/local
-# phpbrew -d ext install gd -- --with-gd-native--ttf --with-jpeg-dir=/usr/local --with-png-dir=/usr/local --with-zlib-dir=/usr/local --with-freetype-dir=/usr/local
-# source ~/.phpbrew/bashrc
-
-# php-version
-# source $(brew --prefix php-version)/php-version.sh && php-version 5
-
-# Base16 Shell
-BASE16_SCHEME="tomorrow"
-BASE16_SHELL="$HOME/.config/base16-shell/base16-$BASE16_SCHEME.dark.sh"
-[[ -s $BASE16_SHELL ]] && . $BASE16_SHELL
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 export NODE_PATH="$(npm root -g):$NODE_PATH"
@@ -23,19 +8,6 @@ export NODE_PATH="$(npm root -g):$NODE_PATH"
 function precmd() {
   export PATH="$(npm bin):$PATH"
 }
-
-# place this after nvm initialization!
-autoload -U add-zsh-hook
-load-nvmrc() {
-  if [[ -f .nvmrc && -r .nvmrc ]]; then
-    nvm use
-  elif [[ $(nvm version) != $(nvm version default)  ]]; then
-    echo "Reverting to nvm default version"
-    nvm use default
-  fi
-}
-add-zsh-hook chpwd load-nvmrc
-load-nvmrc
 
 # nvm completion
 [[ -r $NVM_DIR/bash_completion ]] && . $NVM_DIR/bash_completion
