5b30f77c15fc1efc1c54a60f95efc5d62e9ed736
David Merrique <david.merrique@gmail.com>
2016-10-06 12:40:42 -0400

    Hyper tweak

--- home/.hyper.js
+++ home/.hyper.js
@@ -2,6 +2,7 @@ module.exports = {
   config: {
     fontSize: 18,
     fontFamily: '"Operator Mono", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    backgroundColor: 'rgba(40, 42, 54, 0.8)',
     termCSS: `
       body {
         font-feature-settings: "liga" 0;
@@ -12,10 +13,8 @@ module.exports = {
     summonShortcut: 'Control+`'
   },
   plugins: [
-    'hyperterm-title',
     'hyperterm-snazzy',
-    'hyperterm-summon',
-    'hyperline'
+    'hyperterm-summon'
   ],
   localPlugins: []
 };
