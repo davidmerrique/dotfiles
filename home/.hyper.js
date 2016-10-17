48bfa2546d9188af61224ae9cb9e741091e27114
David Merrique <david.merrique@gmail.com>
2016-10-17 11:56:26 -0400

    Fira Code

--- home/.hyper.js
+++ home/.hyper.js
@@ -1,7 +1,7 @@
 module.exports = {
   config: {
     fontSize: 18,
-    fontFamily: '"Operator Mono", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: '"Fira Code", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
     backgroundColor: 'rgba(40, 42, 54, 0.8)',
     termCSS: `
       body {
