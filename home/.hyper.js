689f0e97e806759b76c25eabaa7983838d8ac657
David Merrique <david.merrique@gmail.com>
2016-10-20 15:08:55 -0400

    Custom Hyper colors

--- home/.hyper.js
+++ home/.hyper.js
@@ -1,21 +1,57 @@
+const backgroundColor = '#252526';
+const foregroundColor = '#eff0eb';
+const red = '#ff5c57';
+const green = '#5af78e';
+const yellow = '#f3f99d';
+const blue = '#57c7ff';
+const magenta = '#ff6ac1';
+const cyan = '#9aedfe';
+const white = '#f1f1f0';
+const lightBlack = '#686868';
+
+const colors = {
+  black: backgroundColor,
+  red,
+  green,
+  yellow,
+  blue,
+  magenta,
+  cyan,
+  white,
+  lightBlack,
+  lightRed: red,
+  lightGreen: green,
+  lightYellow: yellow,
+  lightBlue: blue,
+  lightMagenta: magenta,
+  lightCyan: cyan,
+  lightWhite: foregroundColor
+};
+
 module.exports = {
   config: {
+    colors,
+    backgroundColor,
+    foregroundColor,
+    borderColor: '#222430',
+    cursorColor: '#97979b',
     fontSize: 18,
     fontFamily: '"Fira Code", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
-    backgroundColor: 'rgba(40, 42, 54, 0.8)',
     termCSS: `
-      body {
-        font-feature-settings: "liga" 0;
+      x-screen x-row {
+        font-variant-ligatures: initial;
       }
     `,
     shell: 'zsh',
     cursorShape: 'BEAM',
     summonShortcut: 'Control+`',
-    copyOnSelect: true
+    hyperClean: {
+      hideFirstTabBorder: true
+    }
   },
   plugins: [
+    'hyperclean',
     'hyperterm-title',
-    'hyperterm-snazzy',
     'hyperterm-summon'
   ],
   localPlugins: []
