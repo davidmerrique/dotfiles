e835d7d20763005c1ab93c66467169b754b8adb9
David Merrique <david.merrique@gmail.com>
2016-12-30 10:10:00 -0500

    Put npm precmd back

--- zsh/vendor.zsh
+++ zsh/vendor.zsh
@@ -6,3 +6,7 @@ rbenv() {
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 export NODE_PATH="$(npm root -g):$NODE_PATH"
+
+function precmd() {
+  export PATH="$(npm bin):$PATH"
+}
