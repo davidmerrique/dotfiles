5439e8d2855daec241fc729627e3af079532367c
David Merrique <david.merrique@gmail.com>
2017-02-28 10:08:50 -0500

    Different hub alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,10 @@
+# Use `hub` as our git wrapper:
+# https://hub.github.com/
+if (( $+commands[hub] ))
+then
+  eval "$(hub alias -s)"
+fi
+
 # GRC colorizes nifty unix tools all over the place
 if (( $+commands[grc] )) && (( $+commands[brew] ))
 then
