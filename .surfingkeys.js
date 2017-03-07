5bf4dc683fd676d791ebc760792449fddffd2456
David Merrique <david.merrique@gmail.com>
2017-03-07 17:43:31 -0500

    Surfingkeys back forward

--- .surfingkeys.js
+++ .surfingkeys.js
@@ -1,2 +1,4 @@
 mapkey('J', '#3Go one tab left', 'RUNTIME("previousTab")');
 mapkey('K', '#3Go one tab right', 'RUNTIME("nextTab")');
+mapkey('H', '#4Go back in history', 'history.go(-1)', {repeatIgnore: true});
+mapkey('L', '#4Go forward in history', 'history.go(1)', {repeatIgnore: true});
