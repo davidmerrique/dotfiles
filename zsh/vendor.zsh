184ba0acd828c6d71e385f40096641765f2b2c1f
David Merrique <david.merrique@gmail.com>
2017-07-11 16:32:38 -0400

    Add npx shell-auto-fallback

--- zsh/vendor.zsh
+++ zsh/vendor.zsh
@@ -7,6 +7,8 @@ export NVM_DIR=$(cd ~/.nvm && pwd -P)
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-function precmd() {
-  export PATH="$(npm bin):$PATH"
-}
+# function precmd() {
+#   export PATH="$(npm bin):$PATH"
+# }
+
+source <(npx --shell-auto-fallback zsh)
