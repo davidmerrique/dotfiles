5ff7e91d63424a803999b3bc44a4564bcebb8aa8
David Merrique <david.merrique@gmail.com>
2018-01-18 17:08:23 -0500

    Chest

--- zsh/vendor.zsh
+++ zsh/vendor.zsh
@@ -14,3 +14,5 @@ export NVM_DIR="$HOME/.nvm"
 source <(npx --shell-auto-fallback zsh)
 
 [ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
+
+export CHEST_DIR="$HOME/Sync/chest"
