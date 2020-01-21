1fd473845c5e2e91f1f18a64d8e3019af2cf8fac
David Merrique <david.merrique@gmail.com>
2020-01-21 15:10:00 -0500

    Tweak updateall function

--- home/.config/fish/functions/updateall.fish
+++ home/.config/fish/functions/updateall.fish
@@ -1,11 +1,25 @@
-function updateall
+function updateall -d "Update the system"
+  echo 'Updating...'
+
   fish -c 'cd $DOTFILES && ./install.sh'
+
+  echo 'Updating Homebrew'
   brew update
   brew upgrade
   brew cleanup
+
+  echo 'Updating Yarn & NPM packages'
   yarn global upgrade
   $DOTFILES/bin/npm-yarn-global-upgrade
+
+  echo 'Updating App Store apps'
   mas upgrade
+  /usr/sbin/softwareupdate -ia
+
+  echo 'Updating Fish shell'
   fisher
   fisher self-update
+  fish_update_completions
+
+  exit 0
 end
