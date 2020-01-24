7c704d81008ec584dbadbdbc2851209547282cf0
David Merrique <david.merrique@gmail.com>
2020-01-24 15:41:10 -0500

    Use aliases

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -8,14 +8,6 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias ......="cd ../../../../.."
 
-alias g="git"
-alias h="history"
-alias j="jobs"
-alias v="vim"
-alias o="open"
-alias oo="open ."
-alias cl="clear"
-
 alias flushdns="dscacheutil -flushcache"
 
 alias osx-hidden-show="defaults write com.apple.finder AppleShowAllFiles -bool true && osx-refresh"
@@ -23,18 +15,21 @@ alias osx-hidden-hide="defaults write com.apple.finder AppleShowAllFiles -bool f
 alias osx-desktop-show="defaults write com.apple.finder CreateDesktop -bool true && osx-refresh"
 alias osx-desktop-hide="defaults write com.apple.finder CreateDesktop -bool false && osx-refresh"
 
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gcam='git commit -am'
-alias gpl='git pull'
-alias gpu='git push'
+if status --is-interactive
+  abbr -a -g g "git"
+  abbr -a -g h "history"
+  abbr -a -g v "vim"
+  abbr -a -g o "open"
+  abbr -a -g oo "open ."
+  abbr -a -g gs 'git status -sb'
+  abbr -a -g gcam 'git commit -am'
+  abbr -a -g gpu 'git push'
+  abbr -a -g gco 'git checkout'
+  abbr -a -g gco 'git checkout'
+  abbr -a -g oops 'git commit --amend -m'
+end
+
 alias glog="git log --all --graph --pretty=format:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
-alias gl='git pull --prune'
-alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias oops='git commit --amend -m'
-alias grb='git rebase -p'
 alias gup='git remote update -p; git merge --ff-only @{u}'
 
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
