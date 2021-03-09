8c77433e847b8f7b71423a78a1220228b7a76fdb
David Merrique <david.merrique@gmail.com>
2021-03-09 08:37:44 -0500

    Update

--- home/.config/fish/functions/updateall.fish
+++ home/.config/fish/functions/updateall.fish
@@ -8,15 +8,14 @@ function updateall -d "Update the system"
   brew upgrade
   brew cleanup
 
-  echo 'Updating Yarn & NPM packages'
-  yarn global upgrade
-  $DOTFILES/bin/npm-yarn-global-upgrade
-
-  echo 'Updating App Store apps'
-  mas upgrade
-  /usr/sbin/softwareupdate -ia
+  echo 'Updating NPM packages'
+  $DOTFILES/bin/npm-global-upgrade
 
   echo 'Updating Fish shell'
   fisher update
   fish_update_completions
+
+  echo 'Updating App Store apps'
+  mas upgrade
+  /usr/sbin/softwareupdate -ia
 end
